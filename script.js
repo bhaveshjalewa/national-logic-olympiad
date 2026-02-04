1b9b455dbbf2dc28e2b18ad1789afacff26371fd
bhaveshjalewa <bhaveshjalewa@gmail.com>
2026-02-04 20:23:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #f2f4f8;
+}
+
+.section {
+  margin: 40px;
+}
+
+#timer {
+  font-size: 20px;
+  color: red;
+  margin: 10px;
+}
+
+table {
+  margin: auto;
+  border-collapse: collapse;
+}
+
+td {
+  width: 25px;
+  height: 25px;
+  border: 1px solid #444;
+  cursor: pointer;
+}
+
+.black-cell {
+  background: black;
+}
+
+.kakuro-black {
+  background: black;
+}
+
+input {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  border: none;
+}
